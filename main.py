275b7ffbefb6e54020789726bd045cc128e76b98
HeyTonny <jackieking@yandex.ru>
2022-12-12 15:30:00 +0400

    change main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ('Hello world!')
+def print_info_user(name, last_name, house, street):
+    print(name)
+    print(last_name)
+    print(house)
+    print(street)
+
+name = input('Введите ваше имя: ')
+last_name = input('Введите вашу фамилию: ')
+house= input('Введите ваш дом: ')
+street = input('Введите вашу улицу: ')
+
+print_info_user(name, last_name, house, street)
+
